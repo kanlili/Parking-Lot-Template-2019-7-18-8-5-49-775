6dfa5ef2ef3dccaf9ad2ce72ec1c83a3e39bf135
kanlili <1907877782@qq.com>
2019-07-18 23:46:03 +0800

    Feat:add controller for AC1

--- src/main/java/com/thoughtworks/parking_lot/Controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/Controller/ParkingLotController.java
@@ -1,7 +1,15 @@
 package com.thoughtworks.parking_lot.Controller;
 
+import com.thoughtworks.parking_lot.Respository.ParkingLotRepository;
+import com.thoughtworks.parking_lot.model.ParkingLot;
+
 @RestController
 @RequestMapping("/parkinglot")
 public class ParkingLotController {
-
+    @Autowired
+    private ParkingLotRepository parkingLotRepository;
+    @PostMapping
+    public ParkingLot addParkinglot(@RequestBody ParkingLot parkingLot){
+        return  parkingLotRepository.save(parkingLot);
+    }
 }
